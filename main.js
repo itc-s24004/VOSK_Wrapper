930fd58cb8b9afd06ec1597bd7d30e2687274556
s24004 <s24004@std.it-college.ac.jp>
2025-08-14 12:39:08 +0900

    自動構築がeできないため一時的に変更

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const child = require("child_process");
 const path = require("path");
 const fs = require("fs");
 const EventEmitter = require("events");
+const { app, BrowserWindow, ipcMain, shell } = require("electron");
+const AdmZip = require("adm-zip");
 
 
 
@@ -13,18 +15,109 @@ exports.run = async (REP) => {
     exports.run = null;
 
     const LOCAL_CONTENT_ROOT = REP.get("LOCAL_CONTENT_ROOT");
-    const MODEL_ROOT = path.join(LOCAL_CONTENT_ROOT, "VOSK_Wrapper/models");
+    /**アプリのローカルコンテンツ */
+    const APP_ROOT = path.join(LOCAL_CONTENT_ROOT, "VOSK_Wrapper");
+    if (!fs.existsSync(APP_ROOT)) fs.mkdirSync(APP_ROOT, {recursive: true});
+    /**モデルの保存パス */
+    const MODEL_ROOT = path.join(APP_ROOT, "models");
     if (!fs.existsSync(MODEL_ROOT)) fs.mkdirSync(MODEL_ROOT, {recursive: true});
+    /**voskの実行環境パス */
+    const VOSK_ROOT = path.join(APP_ROOT, "vosk");
+    if (!fs.existsSync(VOSK_ROOT)) fs.mkdirSync(VOSK_ROOT, {recursive: true});
 
+    const createWindow = REP.get("createWindow");
+
+    const GUI_APP_Launcher = REP.get("GUI_APP_Launcher");
+    await GUI_APP_Launcher.whenReady();
+
+    const appButton = new GUI_APP_Launcher();
+    appButton.icon = `rgba(255, 255, 255, 1) url(${path.join(__dirname, "icon.png")}) center / 100% no-repeat `;
+
+
+    //Node.js準備▼
+    appButton.name = "VOSK: Node-?";
     const NodeManager = REP.get("NodeManager");
+    // const local_node = await NodeManager.get("22.17.1");
     const local_node = await NodeManager.get("18.16.1");
-    if (!local_node) return;
-    // await local_node.npm_install(__dirname);
+    if (!local_node) {
+        appButton.name = "VOSK: Node-Error";
+        return;
+    }
+    appButton.name = "VOSK: Node-OK";
 
-    const vosk_root = path.join(__dirname, "vosk");
-    const voskPath = path.join(vosk_root, "vosk.js");
 
 
+    const packOrigin = path.join(__dirname, "vosk.zip");
+    const VOSK = path.join(VOSK_ROOT, "vosk.js");
+    const APP_V = 1;
+
+
+
+
+    //設定▼
+    const configPath = path.join(APP_ROOT, "config.json");
+    const JsonConfig = REP.get("JsonConfig");
+    const [config, useDefaultConfig, saveConfig] = JsonConfig.load( configPath,
+        {
+            "version": 1,
+            "defaultModel": "",
+            /**初期化 */
+            "init": false,
+            /**zip */
+            "pack": false
+        }
+    );
+
+
+    //初期化▼
+    if (useDefaultConfig || !config.init || config.version != APP_V) {
+
+
+        //VOSK実行環境複製▼
+        fs.rmSync(VOSK_ROOT, {recursive: true});
+        fs.mkdirSync(VOSK_ROOT, {recursive: true});
+
+        appButton.name = "VOSK: 環境-?";
+
+        if (app.isPackaged) {//ビルド後▼
+            if (!fs.existsSync(packOrigin)) return appButton.name = "VOSK: 環境-Error";
+            const data = fs.readFileSync(packOrigin);
+            const packPath = path.join(APP_ROOT, "vosk.zip");
+            fs.writeFileSync(packPath, data);
+
+            const zip = new AdmZip(packPath);
+            let buildStatus = true;
+            zip.extractAllToAsync(APP_ROOT, true, true, (err) => {
+                if (err) buildStatus = false;
+            });
+            if (!buildStatus) return appButton.name = "VOSK: 環境-Error";
+
+        } else {//ビルド前▼
+            const vosk_origin_root = path.join(__dirname, "vosk");
+            fs.readdirSync(vosk_origin_root).forEach(FDName => {
+                const target = path.join(vosk_origin_root, FDName);
+                if (fs.statSync(target).isFile()) {
+                    const data = fs.readFileSync(target);
+                    const fpath = path.join(VOSK_ROOT, FDName);
+                    fs.writeFileSync(fpath, data);
+                }
+            });
+
+            const {code, signal} = await local_node.npm(VOSK_ROOT, ["i"]);
+            if (code != 0 || signal != null) return appButton.name = "VOSK: 環境-Error";
+
+        }
+
+
+        config.init = true;
+        config.version = APP_V;
+        saveConfig();
+        appButton.name = "VOSK: 環境-OK";
+    }
+
+
+
+    //文字起こし機能▼
     class VOSK_Wrapper {
         static get models() {
             return fs.readdirSync(MODEL_ROOT).map(FDName => path.join(MODEL_ROOT, FDName)).filter(FDPath => fs.statSync(FDPath).isDirectory());
@@ -38,8 +131,8 @@ exports.run = async (REP) => {
          * BrowserWindow
          * @param {number} sampleRate 
          */
-        constructor(sampleRate, modelPath) {
-            this.#process = child.spawn(local_node.node, [voskPath, String(sampleRate), modelPath], {cwd: vosk_root, stdio: ["pipe", "pipe", "inherit"]});
+        constructor(sampleRate, modelPath = config.defaultModel) {
+            this.#process = child.spawn(local_node.node, [VOSK, String(sampleRate), modelPath], {cwd: VOSK_ROOT, stdio: ["pipe", "pipe", "inherit"]});
             this.#process.stdout.on("data", (data) => {
                 try {
                     /**@type { import("./vosk/result").VOSK_Wrapper_Result } */
@@ -67,8 +160,6 @@ exports.run = async (REP) => {
          * @property {(result: import("vosk").PartialResults) => void} partialResult
          * @property {(result: import("vosk").RecognitionResults | (import("vosk").SpeakerResults & import("vosk").RecognitionResults)) => void} result
          */
-
-
 
         /**
          * @template {keyof VOSK_CallbackTypes} VOSK_Event
@@ -103,4 +194,86 @@ exports.run = async (REP) => {
     REP.register("VOSK_Wrapper", VOSK_Wrapper);
 
     exports.VOSK_Wrapper = VOSK_Wrapper;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    appButton.name = "VOSK";
+    const content = path.join(__dirname, "content", !app.isPackaged && !config.pack ? "build" : "setting", "index.html");
+
+    /**@type {BrowserWindow} */
+    let window;
+
+    appButton.on("click", (type) => {
+        if (type != "single") return;
+        if (window) {
+            window.focus();
+        } else {
+            window = createWindow();
+            window.loadFile(content);
+            window.on("closed", () => {
+                window = null;
+            });
+        }
+    });
+
+
+
+    let packing = false;
+    ipcMain.handle("VOSK_Wrapper", (ev, EventName, ...args) => {
+        if (!window || window.webContents != ev.sender) return;
+        
+        switch(EventName) {
+            case "openModelDir":
+                shell.showItemInFolder(MODEL_ROOT);
+                break;
+            case "getModels":
+                return VOSK_Wrapper.models;
+            case "setModel":
+                const [ model ] = args;
+                config.defaultModel = model;
+                saveConfig();
+                break;
+
+            case "build":
+                if (config.pack) return 1;
+                if (packing) return 0;
+                packing = true;
+                const zip = new AdmZip();
+                let status = true;
+                console.log(VOSK_ROOT)
+                zip.addLocalFolderAsync(VOSK_ROOT, (success, err) => {
+                    if (!success) status = false;
+                    if (err) status = false;
+                }, "/");
+                if (!status) return -1;
+                zip.writeZip(packOrigin, (err) => {
+                    if (err) status = false;
+                });
+                if (!status) return -1;
+                config.pack = true;
+                saveConfig();
+                packing = false;
+                return 1;
+            case "status":
+                return config.pack ? 1 : packing ? 0 : -1;
+        }
+    });
+
+
+
+
 }
